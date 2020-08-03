4e5b85f09f64043236912aa29ce8dc3ed2684a7b
marmarwin0 <marmarw161@gmail.com>
2020-08-03 16:07:02 +0630

    Foodmenu

---
+++ menu.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Food House</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />    
+	<link href="css/templatemo-style.css" rel="stylesheet" />
+	<style>
+	.parallax {
+		/* The image used */
+		background-image: url("img/simple-house-01.jpg");
+		min-height: 500px;
+		background-attachment: fixed;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;	
+	}
+	</style>
+</head>
+<body> 
+	<div class="container">
+		<div class="parallax">
+			<img src="img/simple-house-logo.png" alt="Logo" class="tm-site-logo" /> 
+			<div class="tm-site-text-box">
+				<h1 class="tm-site-title">Food House</h1>
+				<h6 class="tm-site-description">new restaurant </h6>	
+			</div>
+			<nav class="col-md-6 col-12 tm-nav">
+				<ul class="tm-nav-ul">
+					<li class="tm-nav-li"><a href="index.php" class="tm-nav-link active">Home</a></li>
+					<li class="tm-nav-li"><a href="ManageCategory.php" class="tm-nav-link">ManageCategory</a></li>
+					<li class="tm-nav-li"><a href="managefood.html" class="tm-nav-link">ManageFood</a></li>
+					<li class="tm-nav-li"><a href="discountitem.html" class="tm-nav-link">Discount</a></li>
+				</ul>
+			</nav>	
+		</div>
+	</div>
+</body>
+</html>
