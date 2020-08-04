8de84519c0ae1f04010284d150f1804ad56c8bca
marmarein <68365476+marmarwin0@users.noreply.github.com>
2020-08-04 23:56:34 +0630

    Update home.php

--- home.php
+++ home.php
@@ -21,7 +21,8 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo $row["Category_Name"];
+       echo '<button type="button" class="btn btn-success">'.$row["Category_Name"].'</button>';
+
     }
 } else {
     echo "0 results";
